f156fdf30cfba3558b7cef6961f6ce1f96dd3643
Tanvir Anwar Rafi <tanvir.anwar2@gmail.com>
2020-06-23 16:18:27 +0600

    add screenshots

--- app/src/main/java/com/example/rememberpassword/LoggedInActivity.java
+++ app/src/main/java/com/example/rememberpassword/LoggedInActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     public void onBackPressed() {
@@ -18,4 +31,6 @@ public class LoggedInActivity extends AppCompatActivity {
         startActivity(start);
         finishActivity(0);
     }
+
+
 }
